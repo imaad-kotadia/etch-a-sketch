4f2984d7175bd3579eabc1a688058b21a02e8b0c
imaad-kotadia <imaadkotadia@gmail.com>
2026-04-27 15:27:22 +0400

    Added 256 boxes in 16x16 grid with random colors

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container");
+;
+
+const getRandomColor = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor.padStart(6, '0')}`;
+};
+
+
+for(let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box")
+    div.style.backgroundColor = getRandomColor();
+    div.textContent = i + 1;
+    container.appendChild(div);
+}
