1f03ba67fce5b0366a3420d1b8b67c0bf68dd0b5
imaad-kotadia <imaadkotadia@gmail.com>
2026-04-27 16:03:53 +0400

    Added reset button

--- script.js
+++ script.js
@@ -6,22 +6,45 @@ const getRandomColor = () => {
   return `#${randomColor.padStart(6, '0')}`;
 };
 
-
-for(let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("box")
-    div.style.backgroundColor = getRandomColor();
-    div.textContent = i + 1;
-    div.style.opacity = "0";
-    
-    div.addEventListener('mouseenter', (e) => {
+function createGrid(size) {
+    for(let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.classList.add("box")
         div.style.backgroundColor = getRandomColor();
+        //div.textContent = i + 1;
+        div.style.opacity = "0";
+        div.style.height = (800 / size) + "px";
+        div.style.width = (800 / size) + "px";
+        
+        div.addEventListener('mouseenter', (e) => {
+            div.style.backgroundColor = getRandomColor();
 
-        if (div.style.opacity <= 1) {
-             div.style.opacity = String(parseFloat(div.style.opacity) + 0.1);
-        }
-       
-    });
-    
-    container.appendChild(div);
+            if (div.style.opacity <= 1) {
+                div.style.opacity = String(parseFloat(div.style.opacity) + 0.1);
+            }
+        
+        });
+        
+        container.appendChild(div);
+    }
 }
+
+function clearGrid() {
+    document.querySelectorAll('.box').forEach(el => el.remove());
+}
+
+const reset = document.querySelector("button");
+
+reset.addEventListener('click', () => {
+    clearGrid();
+
+    let size = prompt("Enter a new size for the grid");
+
+    if (size > 100) {
+        size = 100;
+    }
+
+    createGrid(size);
+});
+
+createGrid(16);
